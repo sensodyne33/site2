741aa11968c1456e84f82666914cca7f1f474cd4
Webbie <webdevelopercs976@gmail.com>
2022-04-30 19:56:50 -0400

    set up slug URL and slug view test

--- articles/urls.py
+++ articles/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     #this URL already refers to homepage so we need to modify the project urls.py to include this app url
-    path('', views.article_list)
+    path('', views.article_list),
+    #slug is the type of param passed, and second slug is the name of the param (abc,-,etc)
+    path('<slug:slug>/', views.article_detail),
 ]
