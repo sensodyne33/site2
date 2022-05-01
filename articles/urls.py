6858202b44f6d7aed5b51c527ab434786a431ac6
Webbie <webdevelopercs976@gmail.com>
2022-04-30 20:14:14 -0400

    added name for URLs and then added a namespace for the entire articles URLs so we can specify it on templates

--- articles/urls.py
+++ articles/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path 
 from . import views
 
+#name this whole URL page for articles with an appname
+#so it doesn't create confusion when we have other apps with names such as list or detail
+app_name = 'articles'
+
 urlpatterns = [
     #this URL already refers to homepage so we need to modify the project urls.py to include this app url
-    path('', views.article_list),
+    path('', views.article_list, name="list"),
     #slug is the type of param passed, and second slug is the name of the param (abc,-,etc)
-    path('<slug:slug>/', views.article_detail),
+    path('<slug:slug>/', views.article_detail, name="detail"), #we can give this path a name to include it in href of template
 ]
