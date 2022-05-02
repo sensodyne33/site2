e051a17d4b7e76887b5b90d5df1b7e9db0cf4ab4
Webbie <webdevelopercs976@gmail.com>
2022-05-02 16:55:54 -0400

    added login function for the user

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 # Create your views here.
 def signup_view(request):
@@ -17,3 +17,17 @@ def signup_view(request):
     else:
         form = UserCreationForm()
     return render(request, "accounts/signup.html", {'form': form})
+
+
+def login_view(request):
+    #could be post or get request to login
+    if request.method == 'POST':
+        #authenticate it; validate it for us
+        form = AuthenticationForm(data=request.POST)
+        #log user in if form is valid
+        if form.is_valid():
+            return redirect('articles:list')
+    #if request method is GET, form is new form and we send back this new form
+    else:
+        form = AuthenticationForm()
+    return render(request, 'accounts/login.html', {'form': form})
