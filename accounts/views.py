52a6d850e311b5228abbae72f0af893aac373ec2
Webbie <webdevelopercs976@gmail.com>
2022-05-02 16:46:25 -0400

    modified accounts>views.py to include POST and GET request from forms and added csrf-token to capture data from forms

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def signup_view(request):
-    form = UserCreationForm
+    #need to determine which request the form is sending back
+    if request.method == 'POST':
+        #take the data from form and validate it; check if pw is good
+        form = UserCreationForm(request.POST)
+        #once it's valid, save it
+        if form.is_valid():
+            form.save()
+            #log the user in
+            #send user to article:list
+            return redirect('articles:list')
+    #else it's a GET request
+    else:
+        form = UserCreationForm()
     return render(request, "accounts/signup.html", {'form': form})
