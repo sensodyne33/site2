cc27678e95a8ca65de447c1e0d3b17e268a57754
Webbie <webdevelopercs976@gmail.com>
2022-05-03 14:09:36 -0400

    if user wants to access create post URL, we use decorator to redirect them to login first before they can access this page

--- articles/urls.py
+++ articles/urls.py
@@ -8,6 +8,8 @@ app_name = 'articles'
 urlpatterns = [
     #this URL already refers to homepage so we need to modify the project urls.py to include this app url
     path('', views.article_list, name="list"),
+    #create template for users to create new article and if user wants to view, need log in
+    path('create/', views.article_create, name="create"),
     #slug is the type of param passed, and second slug is the name of the param (abc,-,etc)
     path('<slug:slug>/', views.article_detail, name="detail"), #we can give this path a name to include it in href of template
 ]
--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Articles
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def article_list(request):
@@ -16,3 +17,11 @@ def article_detail(request,slug):
     #we use render to get the request and render it to a template
     #third param is the data we getting from the DB which we want to render to template
     return render(request, 'articles/article_detail.html', {'article': article})
+
+
+#need to make create page require user login using a decorator
+#if user is trying to access create and not logged in use decorator to block access
+#then redirect them to login url
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    return render(request, 'articles/article_create.html')
