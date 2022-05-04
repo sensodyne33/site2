41ef03b995d85eed07148d2928fe7d870b8b4e11
Webbie <webdevelopercs976@gmail.com>
2022-05-04 19:23:13 -0400

    updated homepage to refer to article list

--- site2/urls.py
+++ site2/urls.py
@@ -4,11 +4,17 @@ from . import views # helps us reference URLs from web to views' functions relat
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns #let django handle static files
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns, static
 from django.conf import settings
+from articles import views as article_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls), #if we get URL request that ends in admin go to admin site
     path('about/', views.about), # if we get URL request with about/ we go to views.py>about function
-    path('', views.home), #if we get URL request with nothing, go to views.py and home function
+
+    #update home page to simply list back to articles list by importing article list views 
+    #then we name this home so we can reference it from the base.html
+    path('', article_views.article_list, name="home"), #if we get URL request with nothing, go to views.py and home function
+
     path('articles/', include('articles.urls')), #goes to articles.py and runs those URLS; URLs with /articles
     path('accounts/', include('accounts.urls')), #include all account urls
 ]
